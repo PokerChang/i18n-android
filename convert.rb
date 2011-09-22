18f86258cc693a4d1dd842bdde702e6c332fa668
PokerChang <chang@pokerchang.com>
2011-09-21 17:45:39 -0700

    Added merge

--- convert.rb
+++ convert.rb
@@ -5,7 +5,7 @@ if ARGV.count < 2
   puts banner
   exit
 end
-file   = File.open(ARGV[1], "a")
+file   = File.open(ARGV[1], "r")
 
 xml = File.read(ARGV[0])
 doc= REXML::Document.new(xml)
@@ -13,10 +13,31 @@ doc= REXML::Document.new(xml)
 # order alphabetically
 result = []
 doc.elements.each('/resources/string') do |p|
-  result << ('  ' + p.attributes['name']+': "'+ p.text + '"')
+  result << ('  ' + p.attributes['name']+': "'+ p.text.gsub(/\n/,' ').squeeze(' ') + '"')
 end
-result = result.sort{|a, b| a.downcase <=> b.downcase}
+result = result
+
+
+
+output = []
+header = []
+while (line = file.gets)
+  if line.start_with? ' '
+    output << line.chop
+  else
+    header << line.chop
+  end
+end
+
+result = (output | result).sort{|a, b| a.downcase <=> b.downcase}
+
+routput = File.open(ARGV[1], "w")
+header.each do |r|
+  routput.puts r
+  #puts r
+end
 
 result.each do |r|
-  file.puts r
+  routput.puts r
+  #puts r
 end
